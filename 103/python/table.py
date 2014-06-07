08df4ace027fbdfd21dfb1b982705d81efbc8881
eddiehoyle <eddiepie@gmail.com>
2014-06-07 23:42:01 +1200

    Started on count feature

--- 103/python/table.py
+++ 103/python/table.py
@@ -4,13 +4,16 @@ import re
 import random
 from os import path
 from optparse import OptionParser
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 class LeetDict(dict):
     """
     """
 
-    def __init__(self, _file, random_result=True):
-        super(LeetDict, self).__init__()
+    def __init__(self, _file, random_result=False):
 
         self.random_result = random_result
         self.__table = {}
@@ -32,23 +35,24 @@ class LeetDict(dict):
 
         try:
             result = ""
-            for char in iter(string):
+            for char in str(string):
 
-                if char == " ":
-                    result += " "
-                    continue
+                # Character is alpha numeric
+                if re.match("([a-zA-Z])", char):
+                    index = 0
+                    if self.random_result:
+                        row = self._get_row(char)
+                        index = random.randint(0, (len(row))-1)
+                    result += self._get_char(char, index)
 
-                index = 0
-                if self.random_result:
-                    row = self._get_row(char)
-                    index = random.randint(0, (len(row))-1)
-
-                result += self._get_char(char, index)
+                else:
+                    result += char
 
             return result
 
         except Exception as e:
             self._log(e)
+            return None
 
     def parse_file(self, _file):
         """
@@ -66,6 +70,34 @@ class LeetDict(dict):
                     data[found[0]] = found[1].split()
 
         self.__table = data
+
+    def count(self, string):
+        """
+        """
+
+        try:
+            end_value = 1
+            words_dict = {}
+            words = string.split()
+            for word in words:
+                rows = []
+                for char in str(string):
+                    # Character is alpha numeric
+                    if re.match("([a-zA-Z])", char):
+                        rows.append(self._get_row(char))
+
+                count = 1
+                for row in rows:
+                    count *= len(row)
+
+                words_dict[word] = count
+                end_value += count
+
+            return end_value
+
+        except Exception as e:
+            self._log(e)
+            return None
 
     def _get_row(self, char):
         """
@@ -96,7 +128,7 @@ class LeetDict(dict):
         """
         """
 
-        print message
+        log.info(message)
 
 
 
